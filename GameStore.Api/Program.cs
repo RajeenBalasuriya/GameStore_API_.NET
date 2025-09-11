e78473e79dbadb950393f30076a42433c86f6b1c
RajeenBalasuriya <rajeenbalasuriya1@gmail.com>
2025-09-12 00:33:12 +0530

    add entities with ef core

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,6 +1,9 @@
 using GameStore.Api.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var connString = "Data Source=GameStore.db";
+builder.Services.AddSqlite<GameStore.Api.Data.GameStoreContext>(connString);
 var app = builder.Build();
 
 app.MapGamesEndpoints();
